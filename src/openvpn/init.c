61d9c42fd857f4bc03f2c2768d0bf1cdb519d337
Arne Schwabe <arne@rfc2549.org>
2012-09-13 01:20:56 +0200

    Remove script-security warning

--- src/openvpn/init.c
+++ src/openvpn/init.c
@@ -2481,12 +2481,6 @@ do_option_warnings (struct context *c)
     msg (M_WARN, "NOTE: --connect-timeout option is not supported on this OS");
 #endif
 
-  if (script_security >= SSEC_SCRIPTS)
-    msg (M_WARN, "NOTE: the current --script-security setting may allow this configuration to call user-defined scripts");
-  else if (script_security >= SSEC_PW_ENV)
-    msg (M_WARN, "WARNING: the current --script-security setting may allow passwords to be passed to scripts via environmental variables");
-  else
-    msg (M_WARN, "NOTE: " PACKAGE_NAME " 2.1 requires '--script-security 2' or higher to call user-defined scripts or executables");
 }
 
 static void
