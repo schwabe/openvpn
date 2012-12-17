6a5253664cff7692011016d08c79672198af6d27
Arne Schwabe <arne@rfc2549.org>
2012-12-17 22:05:33 +0100

    Fix problems when creating sockets without knowing AF_INET/AF_INET6

--- src/openvpn/options.c
+++ src/openvpn/options.c
@@ -2488,6 +2488,13 @@ options_postprocess_mutate (struct options *o)
           *ace = ce;
         }
     }
+  else if(!o->remote_list && !o->connection_list)
+    {
+      struct connection_entry *ace;
+      ace = alloc_connection_entry (o, M_USAGE);
+      ASSERT (ace);
+      *ace = o->ce;
+    }
 
   ASSERT (o->connection_list);
   int i;
