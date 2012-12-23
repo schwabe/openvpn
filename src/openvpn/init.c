1384c5bc22bf9eb39fa72d9f3eb72f763dcd1ed5
Arne Schwabe <arne@rfc2549.org>
2012-12-23 18:36:01 +0100

    Add google break pad support (only tested under Android)

--- src/openvpn/init.c
+++ src/openvpn/init.c
@@ -3763,7 +3763,10 @@ close_context (struct context *c, int sig, unsigned int flags)
     {
       if ((flags & CC_USR1_TO_HUP)
 	  || (c->sig->source == SIG_SOURCE_HARD && (flags & CC_HARD_USR1_TO_HUP)))
-	c->sig->signal_received = SIGHUP;
+        {
+          c->sig->signal_received = SIGHUP;
+          c->sig->signal_text = "close_context usr1 to hup";
+        }
     }
 
   if (!(flags & CC_NO_CLOSE))
