4de22ba058fabd150ba97320ccc0f6efa04355c8
Arne Schwabe <arne@rfc2549.org>
2014-10-19 20:44:05 +0200

    Always use a 0.0.0.0/0 default on Android instead of def1
    Certain handset manufactures (like Samsung iirc)  are handling
    0.0.0.0/0 different from the split /1 routes, so push the 0.0.0.0/0
    route to the OS.
    
    Change-Id: I22e4b73e744c765a43bdb29f6e18813d103ea757
    Signed-off-by: Arne Schwabe <arne@rfc2549.org>

--- src/openvpn/options.c
+++ src/openvpn/options.c
@@ -7004,7 +7004,10 @@ add_option(struct options *options, char *p[], bool is_inline, const char *file,
             }
             else if (streq(p[j], "def1"))
             {
+#ifndef TARGET_ANDROID
+                /* Android always uses 0.0.0.0/0, so silently ignore the flag */
                 options->routes->flags |= RG_DEF1;
+#endif
             }
             else if (streq(p[j], "bypass-dhcp"))
             {
