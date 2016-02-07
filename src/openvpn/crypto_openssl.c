fb7b059bd703f01e7d63ab6ed6f2e853496be8b1
Steffan Karger <steffan@karger.me>
2016-02-07 20:47:16 +0100

    Add AEAD cipher support (GCM)
    Add Authenticated Encryption with Additional Data (AEAD) support for
    ciphers, which obviates the need for a separate HMAC step.  The MAC is
    integrated into the cipher and the MAC tag is prepended to the payload.
    
    This patch is inspired by the patch originally submitted by Kenny Root
    on the openvpn-devel mailinglist, but does a number things differently:
     * Don't support XTS (makes no sense for VPN)
     * Don't support CCM (needs extra code to make it actually work)
     * Don't force the user to specify "auth none" (that would break
       tls-auth)
     * Add support for PolarSSL (and change internal API for this)
     * Update openvpn frame size ('link mtu') calculation for AEAD modes
     * Use the HMAC key as an implicit part of the IV to save 8 bytes per
       data channel network packet.
     * Also authenticate the opcode/peer-id as AD in P_DATA_V2 packets.
    
    By using the negotiated HMAC key as an implicit part of the IV for
    AEAD-mode ciphers in TLS mode, we can save (at least) 8 bytes on each
    packet sent.  This is particularly interesting for connections which
    transfer many small packets, such as remote desktop or voip connections.
    
    The current AEAD-mode ciphers (for now GCM) are based on CTR-mode cipher
    operation, which requires the IV to be unique (but does not require
    unpredictability).
    
    IV uniqueness is guaranteed by using a combination of at least 64-bits
    of the HMAC key (unique per TLS session), and a 32-bit packet counter.
    The last 32-bit word of the 128-bit cipher block is not part of the IV,
    but is used as a block counter.
    
    AEAD cipher mode is not available for static key mode, since IV
    uniqueness is harder the guarantee over sessions, and I believe
    supporting AEAD in static key mode too is not worth the extra
    complexity.  Modern setups should simply use TLS mode.
    
    Signed-off-by: Steffan Karger <steffan@karger.me>

--- src/openvpn/crypto_openssl.c
+++ src/openvpn/crypto_openssl.c
@@ -255,12 +255,12 @@ show_available_ciphers ()
   int nid;
 
 #ifndef ENABLE_SMALL
-  printf ("The following ciphers and cipher modes are available\n"
-	  "for use with " PACKAGE_NAME ".  Each cipher shown below may be\n"
-	  "used as a parameter to the --cipher option.  The default\n"
-	  "key size is shown as well as whether or not it can be\n"
-          "changed with the --keysize directive.  Using a CBC mode\n"
-	  "is recommended. In static key mode only CBC mode is allowed.\n\n");
+  printf ("The following ciphers and cipher modes are available for use\n"
+	  "with " PACKAGE_NAME ".  Each cipher shown below may be use as a\n"
+	  "parameter to the --cipher option.  The default key size is\n"
+	  "shown as well as whether or not it can be changed with the\n"
+          "--keysize directive.  Using a CBC or GCM mode is recommended.\n"
+	  "In static key mode only CBC mode is allowed.\n\n");
 #endif
 
   for (nid = 0; nid < 10000; ++nid)	/* is there a better way to get the size of the nid list? */
@@ -272,13 +272,16 @@ show_available_ciphers ()
 #ifdef ENABLE_OFB_CFB_MODE
 	      || cipher_kt_mode_ofb_cfb(cipher)
 #endif
+#ifdef HAVE_AEAD_CIPHER_MODES
+	      || cipher_kt_mode_aead(cipher)
+#endif
 	      )
 	    {
 	      const char *var_key_size =
 		  (EVP_CIPHER_flags (cipher) & EVP_CIPH_VARIABLE_LENGTH) ?
 		       "variable" : "fixed";
-	      const char *ssl_only = cipher_kt_mode_ofb_cfb(cipher) ?
-		  " (TLS client/server mode)" : "";
+	      const char *ssl_only = cipher_kt_mode_cbc(cipher) ?
+		  "" : " (TLS client/server mode)";
 
 	      printf ("%s %d bit default key (%s)%s\n",
 		  translate_cipher_name_to_openvpn(OBJ_nid2sn (nid)),
@@ -497,6 +500,15 @@ cipher_kt_block_size (const EVP_CIPHER *cipher_kt)
 }
 
 int
+cipher_kt_tag_size (const EVP_CIPHER *cipher_kt)
+{
+  if (cipher_kt_mode_aead(cipher_kt))
+    return OPENVPN_AEAD_TAG_LENGTH;
+  else
+    return 0;
+}
+
+int
 cipher_kt_mode (const EVP_CIPHER *cipher_kt)
 {
   ASSERT(NULL != cipher_kt);
@@ -524,6 +536,16 @@ cipher_kt_mode_ofb_cfb(const cipher_kt_t *cipher)
       && !(EVP_CIPHER_flags(cipher) & EVP_CIPH_FLAG_AEAD_CIPHER)
 #endif
     ;
+}
+
+bool
+cipher_kt_mode_aead(const cipher_kt_t *cipher)
+{
+#ifdef HAVE_AEAD_CIPHER_MODES
+  return cipher && (cipher_kt_mode(cipher) == OPENVPN_MODE_GCM);
+#else
+  return false;
+#endif
 }
 
 /*
@@ -567,6 +589,15 @@ cipher_ctx_iv_length (const EVP_CIPHER_CTX *ctx)
   return EVP_CIPHER_CTX_iv_length (ctx);
 }
 
+int cipher_ctx_get_tag (EVP_CIPHER_CTX *ctx, uint8_t *tag_buf, int tag_size)
+{
+#ifdef HAVE_AEAD_CIPHER_MODES
+  return EVP_CIPHER_CTX_ctrl (ctx, EVP_CTRL_GCM_GET_TAG, tag_size, tag_buf);
+#else
+  ASSERT (0);
+#endif
+}
+
 int
 cipher_ctx_block_size(const EVP_CIPHER_CTX *ctx)
 {
@@ -593,6 +624,19 @@ cipher_ctx_reset (EVP_CIPHER_CTX *ctx, uint8_t *iv_buf)
 }
 
 int
+cipher_ctx_update_ad (EVP_CIPHER_CTX *ctx, const uint8_t *src, int src_len)
+{
+#ifdef HAVE_AEAD_CIPHER_MODES
+  int len;
+  if (!EVP_CipherUpdate (ctx, NULL, &len, src, src_len))
+    crypto_msg(M_FATAL, "%s: EVP_CipherUpdate() failed", __func__);
+  return 1;
+#else
+  ASSERT (0);
+#endif
+}
+
+int
 cipher_ctx_update (EVP_CIPHER_CTX *ctx, uint8_t *dst, int *dst_len,
     uint8_t *src, int src_len)
 {
@@ -607,6 +651,31 @@ cipher_ctx_final (EVP_CIPHER_CTX *ctx, uint8_t *dst, int *dst_len)
   return EVP_CipherFinal (ctx, dst, dst_len);
 }
 
+int
+cipher_ctx_final_check_tag (EVP_CIPHER_CTX *ctx, uint8_t *dst, int *dst_len,
+    const uint8_t *tag, size_t tag_len)
+{
+#ifdef HAVE_AEAD_CIPHER_MODES
+  if (!cipher_ctx_set_tag (ctx, tag, tag_len))
+    return 0;
+
+  return cipher_ctx_final (ctx, dst, dst_len);
+#else
+  ASSERT (0);
+#endif
+}
+
+int
+cipher_ctx_set_tag (EVP_CIPHER_CTX *ctx, const uint8_t *tag, size_t tag_len)
+{
+#ifdef HAVE_AEAD_CIPHER_MODES
+  /* Setting a tag does not change the tag, so casting away const... */
+  return EVP_CIPHER_CTX_ctrl (ctx, EVP_CTRL_GCM_SET_TAG, tag_len,
+      (uint8_t *) tag);
+#else
+  ASSERT (0);
+#endif
+}
 
 void
 cipher_des_encrypt_ecb (const unsigned char key[DES_KEY_LENGTH],
