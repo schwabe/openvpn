ab9037b052617d40adc7eb54bd7979eeb8b0f1bd
Arne Schwabe <arne@rfc2549.org>
2017-01-04 19:29:20 +0100

    show the right string for key-direction

--- src/openvpn/options.c
+++ src/openvpn/options.c
@@ -1697,7 +1697,7 @@ show_settings(const struct options *o)
 
 #ifdef ENABLE_CRYPTO
     SHOW_STR(shared_secret_file);
-    SHOW_INT(key_direction);
+    SHOW_PARM(key_direction, keydirection2ascii(o->key_direction, false), "%s");
     SHOW_STR(ciphername);
     SHOW_BOOL(ncp_enabled);
     SHOW_STR(ncp_ciphers);
