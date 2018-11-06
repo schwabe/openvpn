7c059bdc4992c57c82822b0463c2f8329b19086f
Arne Schwabe <arne@rfc2549.org>
2018-11-06 15:14:27 +0100

    fixmda

--- src/openvpn/multi.c
+++ src/openvpn/multi.c
@@ -1714,39 +1714,35 @@ multi_client_connect_post_plugin(struct multi_context *m,
 enum client_connect_return
 multi_client_connect_mda(struct multi_context *m,
                          struct multi_instance *mi,
-                         const struct buffer_list *config,
                          unsigned int *option_types_found)
 {
     enum client_connect_return ret = CC_RET_SKIPPED;
 #ifdef MANAGEMENT_DEF_AUTH
     if (mi->cc_config)
     {
-        if (config)
+        struct buffer_entry *be;
+
+        for (be = mi->cc_config->head; be != NULL; be = be->next)
         {
-            struct buffer_entry *be;
-
-            for (be = config->head; be != NULL; be = be->next)
-            {
-                const char *opt = BSTR(&be->buf);
-                options_string_import(&mi->context.options,
-                                      opt,
-                                      D_IMPORT_ERRORS|M_OPTERR,
-                                      CLIENT_CONNECT_OPT_MASK,
-                                      option_types_found,
-                                      mi->context.c2.es);
-            }
-
-            /*
-             * If the --client-connect script generates a config file
-             * with an --ifconfig-push directive, it will override any
-             * --ifconfig-push directive from the --client-config-dir
-             * directory or any --ifconfig-pool dynamic address.
-             */
-            multi_select_virtual_addr(m, mi);
-            multi_set_virtual_addr_env(m, mi);
-
-            ret = CC_RET_SUCCEEDED;
+            const char *opt = BSTR(&be->buf);
+            options_string_import(&mi->context.options,
+                                  opt,
+                                  D_IMPORT_ERRORS|M_OPTERR,
+                                  CLIENT_CONNECT_OPT_MASK,
+                                  option_types_found,
+                                  mi->context.c2.es);
         }
+
+        /*
+         * If the --client-connect script generates a config file
+         * with an --ifconfig-push directive, it will override any
+         * --ifconfig-push directive from the --client-config-dir
+         * directory or any --ifconfig-pool dynamic address.
+         */
+        multi_select_virtual_addr(m, mi);
+        multi_set_virtual_addr_env(m, mi);
+
+        ret = CC_RET_SUCCEEDED;
     }
 #endif /* ifdef MANAGEMENT_DEF_AUTH */
     return ret;
