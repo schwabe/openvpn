ac5903ed52f200d77d182dcf53010122358771bd
Arne Schwabe <arne@rfc2549.org>
2018-11-29 14:35:36 +0100

    Rewrite auth-token-gen to be based on HMAC based tokens
    The previous auth-token implementation had a serious problem, especially when
    paired with an unpatched OpenVPN client that keeps trying the auth-token
    (commit e61b401a).
    
    The auth-token-gen implementation forgot the auth-token on reconnect, this
    lead to reconnect with auth-token never working.
    
    This new implementation implements the auth-token in a stateles variant. By
    using HMAC to sign the auth-token the server can verify if a token has been
    authenticated and by checking the embedded timestamp in the token it can
    also verify that the auth-token is still valid.

--- src/openvpn/init.c
+++ src/openvpn/init.c
@@ -49,6 +49,7 @@
 #include "ssl_verify.h"
 #include "tls_crypt.h"
 #include "forward-inline.h"
+#include "auth_token.h"
 
 #include "memdbg.h"
 
@@ -1088,6 +1089,16 @@ do_genkey(const struct options *options)
         return true;
     }
 #endif
+    if (options->auth_token_gen_secret_file)
+    {
+        if (!options->auth_token_secret_file)
+        {
+            msg(M_USAGE, "--auth-gen-token-secret-genkey requires a server key "
+                "to be set via --auth-gen-token-secret to create a shared secret");
+        }
+        auth_token_write_server_key_file(options->auth_token_secret_file);
+        return true;
+    }
     return false;
 }
 
@@ -2494,7 +2505,6 @@ init_crypto_pre(struct context *c, const unsigned int flags)
         rand_ctx_enable_prediction_resistance();
     }
 #endif
-
 }
 
 /*
@@ -2561,6 +2571,20 @@ do_init_crypto_static(struct context *c, const unsigned int flags)
     /* Sanity check on IV, sequence number, and cipher mode options */
     check_replay_iv_consistency(&c->c1.ks.key_type, options->replay,
                                 options->use_iv);
+}
+
+/*
+ * Initialise the auth-token key context
+ */
+static void
+do_init_auth_token_key(struct context *c)
+{
+    if (!c->options.auth_token_generate)
+        return;
+
+    auth_token_init_secret(&c->c1.ks.auth_token_key,
+                           c->options.auth_token_secret_file,
+                           c->options.auth_token_secret_file_inline);
 }
 
 /*
@@ -2641,6 +2665,9 @@ do_init_crypto_tls_c1(struct context *c)
             tls_crypt_init_key(&c->c1.ks.tls_wrap_key, options->tls_crypt_file,
                                options->tls_crypt_inline, options->tls_server);
         }
+
+        /* initialise auth-token crypto support */
+        do_init_auth_token_key(c);
 
 #if 0 /* was: #if ENABLE_INLINE_FILES --  Note that enabling this code will break restarts */
         if (options->priv_key_file_inline)
@@ -2803,6 +2830,7 @@ do_init_crypto_tls(struct context *c, const unsigned int flags)
     to.auth_user_pass_file = options->auth_user_pass_file;
     to.auth_token_generate = options->auth_token_generate;
     to.auth_token_lifetime = options->auth_token_lifetime;
+    to.auth_token_key = c->c1.ks.auth_token_key;
 #endif
 
     to.x509_track = options->x509_track;
@@ -4419,6 +4447,9 @@ inherit_context_child(struct context *dest,
     dest->c1.authname = src->c1.authname;
     dest->c1.keysize = src->c1.keysize;
 #endif
+
+    /* inherit auth-token */
+    dest->c1.ks.auth_token_key = src->c1.ks.auth_token_key;
 
     /* options */
     dest->options = src->options;
