ccc67364db2e24c03c0f22c07770800890602ae8
Arne Schwabe <arne@openvpn.net>
2018-12-19 15:14:56 +0100

    Implement a permanent session id in auth-token
    This allows an external authentication method
    (e.g. management interface) to track the connection and distinguish a
    reconnection from multiple connections.
    
    Addtionally this now also checks to workaround a problem with
    OpenVPN 3 core that sometimes uses a username hint from the config
    instead of using an empty username if the token would be valid
    with an empty username. Accepting such token can only be explicitly
    done if the auth keyword to auth-gen-token is present.

--- src/openvpn/options.h
+++ src/openvpn/options.h
@@ -460,7 +460,8 @@ struct options
     bool auth_user_pass_verify_script_via_file;
     bool auth_token_generate;
     bool auth_token_gen_secret_file;
-    unsigned int auth_token_lifetime;
+    bool auth_token_call_auth;
+    int auth_token_lifetime;
     const char *auth_token_secret_file;
     const char *auth_token_secret_file_inline;
 
