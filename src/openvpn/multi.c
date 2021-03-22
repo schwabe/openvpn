169471804c762745da31223148db52176ce1a43d
Arne Schwabe <arne@rfc2549.org>
2021-03-22 17:36:27 +0100

    Make waiting on auth an explicit state in the context state machine
    Previously we relied on checking tls_authentication_status to check
    wether to determine if the context auth state is actually valid or not.
    This patch eliminates that check by introducing waiting on the
    authentication as extra state in the context auth, state machine.
    
    Signed-off-by: Arne Schwabe <arne@rfc2549.org>

--- src/openvpn/multi.c
+++ src/openvpn/multi.c
@@ -2596,11 +2596,6 @@ static const multi_client_connect_handler client_connect_handlers[] = {
 static void
 multi_connection_established(struct multi_context *m, struct multi_instance *mi)
 {
-    if (tls_authentication_status(mi->context.c2.tls_multi) != TLS_AUTHENTICATION_SUCCEEDED)
-    {
-        return;
-    }
-
     /* We are only called for the CAS_PENDING_x states, so we
      * can ignore other states here */
     bool from_deferred = (mi->context.c2.tls_multi->multi_state != CAS_PENDING);
