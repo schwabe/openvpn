5e686c14c64528a195dc2d78588215910dd09291
Arne Schwabe <arne@rfc2549.org>
2023-07-01 15:53:02 +0300

    Fix print peer information not compiling with wolfSSL
    wolfSSL is missing this API calls that are support since OpenSSL 1.0.2.
    
    Change-Id: Ifabd9feb6f7ec86fe0b45ed9a8512c7b0daaf7ea
    Signed-off-by: Arne Schwabe <arne@rfc2549.org>

--- src/openvpn/ssl_openssl.c
+++ src/openvpn/ssl_openssl.c
@@ -2153,6 +2153,17 @@ print_cert_details(X509 *cert, char *buf, size_t buflen)
     EVP_PKEY_free(pkey);
 }
 
+#if defined(ENABLE_CRYPTO_WOLFSSL)
+/* wolfSSL does not implement these APIs */
+static void
+print_peer_signature(SSL *ssl, char *buf, size_t buflen)
+{
+}
+static void
+print_server_tempkey(SSL *ssl, char *buf, size_t buflen)
+{
+}
+#else  /* if defined(ENABLE_CRYPTO_WOLFSSL) */
 static void
 print_server_tempkey(SSL *ssl, char *buf, size_t buflen)
 {
@@ -2246,6 +2257,7 @@ print_peer_signature(SSL *ssl, char *buf, size_t buflen)
     snprintf(buf, buflen, ", peer signing digest/type: %s %s",
              peer_sig, peer_sig_type);
 }
+#endif /* if defined(ENABLE_CRYPTO_WOLFSSL) */
 
 
 
