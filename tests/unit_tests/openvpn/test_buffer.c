d9b95dbdca6c50b9c7e30ca05dd71e5534597f38
Arne Schwabe <arne@rfc2549.org>
2023-07-28 13:42:29 +0200

    Add helper method to read an integer from a buffer
    Change-Id: Ifdd2122a3e60cdb11cc9df67570ab5628bfcdeea
    Signed-off-by: Arne Schwabe <arne@rfc2549.org>

--- tests/unit_tests/openvpn/test_buffer.c
+++ tests/unit_tests/openvpn/test_buffer.c
@@ -449,7 +449,36 @@ test_buffer_chomp(void **state)
     /* Check that our own method agrees */
     assert_true(string_check_buf(&buf2, CC_PRINT | CC_NULL, CC_CRLF));
     assert_string_equal(BSTR(&buf2), "CR_RESPONSE,MTIx");
+    gc_free(&gc);
+}
 
+static void
+test_buffer_read_int(void **state)
+{
+    struct gc_arena gc = gc_new();
+    struct buffer buf = alloc_buf_gc(1000, &gc);
+
+    buf_printf(&buf, "72732,1234");
+
+    int tmp = -1;
+    assert_true(buffer_read_int(&buf, &tmp));
+    assert_int_equal(tmp, 72732);
+    assert_int_equal(*BPTR(&buf), ',');
+
+    buf_advance(&buf, 1);
+
+    assert_true(buffer_read_int(&buf, &tmp));
+    assert_int_equal(tmp, 1234);
+    assert_int_equal(buf_len(&buf), 0);
+
+    buf = alloc_buf_gc(1000, &gc);
+    buf_printf(&buf, "fo42,7777");
+
+    assert_false(buffer_read_int(&buf, &tmp));
+
+    buf = alloc_buf_gc(1000, &gc);
+    buf_printf(&buf, "");
+    assert_false(buffer_read_int(&buf, &tmp));
     gc_free(&gc);
 }
 
@@ -488,7 +517,8 @@ main(void)
         cmocka_unit_test(test_character_class),
         cmocka_unit_test(test_character_string_mod_buf),
         cmocka_unit_test(test_snprintf),
-        cmocka_unit_test(test_buffer_chomp)
+        cmocka_unit_test(test_buffer_chomp),
+        cmocka_unit_test(test_buffer_read_int)
     };
 
     return cmocka_run_group_tests_name("buffer", tests, NULL, NULL);
