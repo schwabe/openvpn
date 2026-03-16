5053c7f6cb1f03588b4ed41114da974334bf40f8
Arne Schwabe <arne@rfc2549.org>
2026-03-16 20:00:26 +0100

    Do not support tls_ctx_set_cert_profile on AWS-LC
    SSL_CTX_set_security_level does nothing on AWS-LC and gives a deprecated
    warning on compile. It is better to give the user a warning than to
    effectively silently ignore it as well.
    
    Change-Id: I74841d3611c62d3c59fc839bc73a0c83ce025262
    Signed-off-by: Arne Schwabe <arne@rfc2549.org>

--- src/openvpn/ssl_openssl.c
+++ src/openvpn/ssl_openssl.c
@@ -518,8 +518,9 @@ tls_ctx_restrict_ciphers_tls13(struct tls_root_ctx *ctx, const char *ciphers)
 void
 tls_ctx_set_cert_profile(struct tls_root_ctx *ctx, const char *profile)
 {
-#if OPENSSL_VERSION_NUMBER > 0x10100000L \
-    && (!defined(LIBRESSL_VERSION_NUMBER) || LIBRESSL_VERSION_NUMBER > 0x3060000fL)
+#if OPENSSL_VERSION_NUMBER > 0x10100000L                                            \
+    && (!defined(LIBRESSL_VERSION_NUMBER) || LIBRESSL_VERSION_NUMBER > 0x3060000fL) \
+    && !defined(OPENSSL_IS_AWSLC)
     /* OpenSSL does not have certificate profiles, but a complex set of
      * callbacks that we could try to implement to achieve something similar.
      * For now, use OpenSSL's security levels to achieve similar (but not equal)
@@ -549,7 +550,7 @@ tls_ctx_set_cert_profile(struct tls_root_ctx *ctx, const char *profile)
     if (profile)
     {
         msg(M_WARN,
-            "WARNING: OpenSSL 1.1.0 and LibreSSL do not support "
+            "WARNING: OpenSSL 1.1.0, aws-lc and LibreSSL do not support "
             "--tls-cert-profile, ignoring user-set profile: '%s'",
             profile);
     }
